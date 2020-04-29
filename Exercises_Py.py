92abe13981c2d68cdcd53049d5c480102312febf
cbirkho <connor_birkhold@gap.com>
2020-04-29 15:55:15 -0700

    update

--- Exercises_Py.py
+++ Exercises_Py.py
@@ -1,7 +1,8 @@
-
+#%%
 # load packages
 import pandas as pd 
 
+#%%
 #==============================================================================================================================================================
 # Load the data
 d_customer = pd.read_excel('./data/tables.xlsx', sheet_name='d_customer')
@@ -13,6 +14,7 @@ product_table = pd.read_excel('./data/tables.xlsx', sheet_name='product_table')
 # clean up the column names for line_sales. Replacing spaces with underscores
 line_sales.columns = [c.replace(" ", "_") for c in line_sales.columns]
 
+#%%
 #==============================================================================================================================================================
 # Check the structure of the data
 d_customer.head()
@@ -24,14 +26,15 @@ line_sales.dtypes
 product_table.head()
 product_table.dtypes
 
+#%%
 #==============================================================================================================================================================
 # 1. Write code to provide a list of clients that purchased the brand 'king' and have an email address
 
 # lets define a function to join our three data sets together
 def joined_data(sales_data, product_data, customer_data):
-    '''
-    Joins are three data sets together
-    '''
+    """
+    Joins the three data sets together
+    """
     prod_info = pd.merge(sales_data, product_data, how='left', left_on='product_key', right_on='product_key')
     return pd.merge(prod_info, customer_data, how='left', left_on='customer_id', right_on='customer_id')
 
@@ -42,6 +45,7 @@ total_tab = total_tab[total_tab['email_address'].notnull()] # filter to non NULL
 
 print(total_tab[['customer_id', 'email_address']]) # print the list
 
+#%%
 #==============================================================================================================================================================
 # 2. Write code to provide three lists
 
@@ -79,6 +83,7 @@ three_lists(line_sales, ['store'], 1)
 # C. One list of distinct clients that purchased from both the online and store channel
 three_lists(line_sales, ['store', 'online'], 2)
 
+#%%
 #==============================================================================================================================================================
 # 3. Write code to provide the count of distinct clients, total orders, total sales, and average purchase by purchase channel
 total_tab = joined_data(sales_data=line_sales, product_data=product_table, customer_data=d_customer)
@@ -87,19 +92,31 @@ total_tab = joined_data(sales_data=line_sales, product_data=product_table, custo
     agg({'customer_id': 'nunique', 'order_number':'nunique', 'total_amt': ['sum', 'mean']})
 )
 
-
+#%%
 #==============================================================================================================================================================
 # 4. Write code to provide a list of distinct clients that have an email address, signed up for email after 01/01/2010, and purchased tops after 3/10/2010
 total_tab = joined_data(sales_data=line_sales, product_data=product_table, customer_data=d_customer)
 
-import datetime as dt
-total_tab.set_index('signup_date') # set my index as the `signup_date` for easy filtering
+total_tab = total_tab[total_tab.email_address.notnull()] # filter out NaN in email_address
 
-dt_filter = dt.datetime(2010, 1, 1)
-total_tab[:dt_filter]
+total_tab['signup_date'] = total_tab['signup_date'].replace(["not BI"], [None]) # replace "not BI" with None
+total_tab = total_tab[total_tab.signup_date.notnull()] # filter out None in signup_date
 
+classFilt = total_tab['class'] == 'tops' # create boolean series to use as filter
+total_tab = total_tab[classFilt] # filter using boolean 
 
+total_tab = total_tab[total_tab['order_date'] > "2010-03-10"] # filter to orders after 2010-03-10
 
+total_tab['signup_date'] = pd.to_datetime(total_tab['signup_date']) # convert signup_date to datetime
+total_tab = total_tab[total_tab['signup_date'] > "2010-01-01"] # filter to signups after 2010-01-01
 
+print(total_tab) # no transactions meet these requirements
+
+#%%
 #==============================================================================================================================================================
 # 5. Write code to provide a list of each email address and each email addresses total sales
+total_tab = joined_data(sales_data=line_sales, product_data=product_table, customer_data=d_customer)
+
+total_tab.groupby('email_address').agg({'total_amt': 'sum'}) # excludes NaN in email_address
+
+# %%
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#### The code and data in this repository is aimed at providing users with an opportunity to practice basic coding techniques in various languages to solve problems. It is best suited for an individual who is just beginning their journey in data science/learning a new programming language. I've solved these problems in one way, but there are multiple ways to do it. Let these examples serve as a jumping off point.
+#### The code and data in this repository is aimed at providing users with an opportunity to practice basic coding techniques in various languages to solve problems. It is best suited for an individual who is just beginning their journey in data science/learning a new programming language. 
 
 #### For this exercise we are using the data `/data/tables.xlsx` to answer the following questions:
 1. Write code to provide a list of clients that purchased the brand 'king' and have an email address
